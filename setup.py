11ea56ab140b873471d305c66e2f7de1307d36d9
Stefano Esposito <stefano.esposito97@outlook.com>
2025-02-18 12:22:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-
+import os
 import os.path as osp
 
-
 ROOT = osp.dirname(osp.abspath(__file__))
+
+CUDA_HOME = os.environ["CUDA_HOME"]
+if CUDA_HOME is None:
+    raise ValueError("Please set CUDA_HOME environment variable to the path of your CUDA installation")
 
 setup(
     name='lietorch',
@@ -16,24 +19,36 @@ setup(
         CUDAExtension('lietorch_backends', 
             include_dirs=[
                 osp.join(ROOT, 'lietorch/include'), 
-                osp.join(ROOT, 'eigen')],
+                osp.join(ROOT, 'eigen'),
+                f"{CUDA_HOME}/include"
+            ],
+            library_dirs=[f"{CUDA_HOME}/lib"],
+            libraries=['cudart'],
             sources=[
                 'lietorch/src/lietorch.cpp', 
                 'lietorch/src/lietorch_gpu.cu',
-                'lietorch/src/lietorch_cpu.cpp'],
+                'lietorch/src/lietorch_cpu.cpp'
+            ],
             extra_compile_args={
                 'cxx': ['-O2'], 
-                'nvcc': ['-O2',
-                    '-gencode=arch=compute_60,code=sm_60', 
-                    '-gencode=arch=compute_61,code=sm_61', 
-                    '-gencode=arch=compute_70,code=sm_70', 
+                'nvcc': [
+                    '-O2',
+                    '-gencode=arch=compute_60,code=sm_60',
+                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_70,code=sm_70',
                     '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_75,code=compute_75',
-                    
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',
+                    '-gencode=arch=compute_89,code=sm_89',
+                    '-gencode=arch=compute_90,code=sm_90',
+                    '-gencode=arch=compute_90,code=compute_90'
                 ]
             }),
 
         CUDAExtension('lietorch_extras', 
+            include_dirs=[f"{CUDA_HOME}/include"],
+            library_dirs=[f"{CUDA_HOME}/lib"],
+            libraries=['cudart'],
             sources=[
                 'lietorch/extras/altcorr_kernel.cu',
                 'lietorch/extras/corr_index_kernel.cu',
@@ -44,17 +59,21 @@ setup(
             ],
             extra_compile_args={
                 'cxx': ['-O2'], 
-                'nvcc': ['-O2',
-                    '-gencode=arch=compute_60,code=sm_60', 
-                    '-gencode=arch=compute_61,code=sm_61', 
-                    '-gencode=arch=compute_70,code=sm_70', 
+                'nvcc': [
+                    '-O2',
+                    '-gencode=arch=compute_60,code=sm_60',
+                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_70,code=sm_70',
                     '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_75,code=compute_75',
-                    
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',
+                    '-gencode=arch=compute_89,code=sm_89',
+                    '-gencode=arch=compute_90,code=sm_90',
+                    '-gencode=arch=compute_90,code=compute_90'
                 ]
             }),
     ],
-    cmdclass={ 'build_ext': BuildExtension }
+    cmdclass={'build_ext': BuildExtension}
 )
 
 
